09f4667b451ccc3a14a1d2e5d81365564ff2c185
jaelyung <jaekim7724@gmail.com>
2023-11-17 09:27:52 +0900

    [chore] 프로젝트 파일 최초생성

---
+++ MyMomii-admin/MyMomii-admin/MyMomii_adminApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMomii_adminApp.swift
+//  MyMomii-admin
+//
+//  Created by jaelyung kim on 11/17/23.
+//
+
+import SwiftUI
+
+@main
+struct MyMomii_adminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
